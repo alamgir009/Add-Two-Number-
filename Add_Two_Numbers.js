a75a91a6f328ca8d0ee227158b6e0e6b080f1dbd
Alamgir Islam <alamgirislam009@gmail.com>
2023-08-05 20:07:00 +0530

    first commit

---
+++ Add_Two_Numbers.js
@@ -0,0 +1,21 @@
+// let l1 = [9,9,9,9,9,9,9]
+// let l2 = [9,9,9,9]
+// let rev1 = 0;
+// let rev2 = 0;
+
+// for(let i = l1.length-1; i >= 0; i--){
+//     rev1 = rev1 * 10 + l1[i];
+// }
+// for(let i = l2.length-1; i >= 0; i--){
+//     rev2 = rev2 * 10 + l2[i];
+// }
+
+// let num = rev1 + rev2;
+// let v = [];
+// while(num > 0){
+//     v.push(num % 10);
+//     num = parseInt(num / 10);
+// }
+// console.log(v);
+
+let addTwoNumbers = function
